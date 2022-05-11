5fe5a4a1e021bf4ae76cca8372eb7057b9e4929c
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-11 20:54:06 -0300

    updated: update filename and return a new link

--- controllers/s3Controllers.js
+++ controllers/s3Controllers.js
@@ -1,51 +1,77 @@
-const {uploadFile,downloadFile,deleteFile,updatedFile} = require("../services/s3")
-const ErrorResponse = require("../utils/errorResponse")
-const httpCodes = require("../constants/httpCodes")
+const {
+  uploadFile,
+  downloadFile,
+  deleteFile,
+  updatedFile,
+  getUrlFile,
+} = require("../services/s3");
+const ErrorResponse = require("../utils/errorResponse");
+const httpCodes = require("../constants/httpCodes");
 
-const uploadS3 = async (req,res,next)=>{
-    const file = req.files.file
-    uploadFile(file,(error,data)=>{
-        if(error){
-            return next(new ErrorResponse("Something went wrong.",httpCodes.BAD_REQUEST))
-        }
-        return res.status(httpCodes.OK).json({error:false,message:"File uploaded successfully"});
-    })
-}
-
-const downloadS3 = async (req,res,next)=>{
-    try {
-        const fileToSend = await downloadFile(req.query.filename)
-        console.log(fileToSend)
-        fileToSend.pipe(res.download)
-    } catch (error) {
-        next(error)
+const uploadS3 = async (req, res, next) => {
+  const file = req.files.file;
+  uploadFile(file, (error, data) => {
+    if (error) {
+      return next(
+        new ErrorResponse("Something went wrong.", httpCodes.BAD_REQUEST)
+      );
     }
-}
+    return res
+      .status(httpCodes.OK)
+      .json({ error: false, message: "File uploaded successfully" });
+  });
+};
 
-const deleteS3 = (req,res,next)=>{
-    deleteFile(req.body.filename,(error,data)=>{
-        if(error){
-            return next(new ErrorResponse("Can not delete file, Please try again later",httpCodes.BAD_REQUEST))
-        }
-        return res.status(httpCodes.OK).json({error:false,message:"File has been deleted successfully"});
-    })
-}
+const downloadS3 = async (req, res, next) => {
+  try {
+    const fileToSend = await downloadFile(req.query.filename);
+    fileToSend.pipe(res);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const updateS3 = (req,res,next)=>{
-    const {newKey,oldKey} = req.body
-    updatedFile(newKey,oldKey,(error,data)=>{
-        if(error){
-            return next(new ErrorResponse("Can not modify the file, Please try again later",httpCodes.BAD_REQUEST))
-        }
-        req.body.filename = oldKey
-        deleteS3(req.body.filename,(error,data)=>{
-            if(error){
-                return next(new ErrorResponse("Can not modify the file, Please try again later",httpCodes.BAD_REQUEST))
-            }
-            return res.status(httpCodes.OK).json({error:false,message:"File has been changed successfully"});
-        })
-    })
-}
+const deleteS3 = (req, res, next) => {
+  deleteFile(req.body.filename, (error, data) => {
+    if (error) {
+      return next(
+        new ErrorResponse(
+          "Can not delete file, Please try again later",
+          httpCodes.BAD_REQUEST
+        )
+      );
+    }
+    return res
+      .status(httpCodes.OK)
+      .json({ error: false, message: "File has been deleted successfully" });
+  });
+};
 
+const updateS3 = (req, res, next) => {
+  const { newKey, oldKey } = req.body;
+  updatedFile(newKey, oldKey, (error, data) => {
+    if (error) {
+      return next(
+        new ErrorResponse(
+          "Can not modify the file, Please try again later",
+          httpCodes.BAD_REQUEST
+        )
+      );
+    }
+    req.body.filename = oldKey;
+    deleteS3(req.body.filename, (error, data) => {
+      if (error) {
+        return next(
+          new ErrorResponse(
+            "Can not modify the file, Please try again later",
+            httpCodes.BAD_REQUEST
+          )
+        );
+      }
+      const url = getUrlFile(newKey);
+      return res.status(httpCodes.OK).json({ error: false, newUrl: url });
+    });
+  });
+};
 
-module.exports = {deleteS3,downloadS3,uploadS3,updateS3}
+module.exports = { deleteS3, downloadS3, uploadS3, updateS3 };
--- services/s3.js
+++ services/s3.js
@@ -1,64 +1,77 @@
-require("dotenv").config()
-const fs = require('fs');
-const aws = require('aws-sdk');
+require("dotenv").config();
+const fs = require("fs");
+const aws = require("aws-sdk");
 
-const bucketName = process.env.AWS_S3_BUCKET
-const accessKeyId = process.env.AWS_S3_KEY
-const secretAccessKey = process.env.AWS_S3_SECRET
-const region = process.env.AWS_S3_REGION
+const bucketName = process.env.AWS_S3_BUCKET;
+const accessKeyId = process.env.AWS_S3_KEY;
+const secretAccessKey = process.env.AWS_S3_SECRET;
+const region = process.env.AWS_S3_REGION;
 
 aws.config.update({
-    region,
-    accessKeyId,
-    secretAccessKey
+  region,
+  accessKeyId,
+  secretAccessKey,
 });
 
-const s3 = new aws.S3()
+const s3 = new aws.S3();
 
+const uploadFile = (file, next) => {
+  const fileStream = fs.createReadStream(file.tempFilePath);
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.name,
+  };
+  s3.upload(uploadParams, (error, data) => {
+    console.log(data, error);
+    next(error, data);
+  });
+};
 
-const uploadFile = (file,next) => {
-    const fileStream = fs.createReadStream(file.tempFilePath)
-    const uploadParams = {
-        Bucket: bucketName,
-        Body: fileStream,
-        Key: file.name
+const downloadFile = (key) => {
+  const downloadParams = {
+    Key: key,
+    Bucket: bucketName,
+  };
+  return s3.getObject(downloadParams).createReadStream();
+};
 
+const deleteFile = (key, next) => {
+  const deleteParams = {
+    Key: key,
+    Bucket: bucketName,
+  };
+  s3.deleteObject(deleteParams, (error, data) => {
+    next(error, data);
+  });
+};
+
+const updatedFile = (oldKey, newKey, next) => {
+  s3.copyObject(
+    {
+      Bucket: bucketName,
+      CopySource: `${bucketName}${oldKey}`,
+      Key: newKey,
+    },
+    (error, data) => {
+      next(error, data);
     }
-    s3.upload(uploadParams,(error,data)=>{
-        console.log(data,error)
-        next(error,data)
-    })
-}
+  );
+};
 
-const downloadFile = key=>{
-    const downloadParams = {
-        Key:key,
-        Bucket:bucketName
-    }
-    return s3.getObject(downloadParams).createReadStream();
-}
+const getUrlFile = (key) => {
+  const signedUrl = s3.getSignedUrl("getObject", {
+    Key: key,
+    Bucket: bucketName,
+    Expires: 900,
+  });
+  return signedUrl;
+};
 
-const deleteFile = (key,next)=>{
-    const deleteParams = {
-        Key:key,
-        Bucket:bucketName
-    };
-    s3.deleteObject(deleteParams,(error,data)=>{
-        next(error,data);
-    });
-}
-
-const updatedFile = (oldKey,newKey,next)=>{
-    s3.copyObject({
-        Bucket: bucketName, 
-        CopySource: `${bucketName}${oldKey}`, 
-        Key: newKey
-       },(error,data)=>{
-           next(error,data)
-       })
-       
-}
-
-
-
-module.exports = {uploadFile,downloadFile,deleteFile,updatedFile}
+module.exports = {
+  uploadFile,
+  downloadFile,
+  deleteFile,
+  updatedFile,
+  getUrlFile,
+};
