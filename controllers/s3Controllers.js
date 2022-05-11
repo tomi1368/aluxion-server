f47c2769a69560a5d8cea7d0517ae30dd6674c6d
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-11 20:05:56 -0300

    update: update s3 file

--- controllers/s3Controllers.js
+++ controllers/s3Controllers.js
@@ -1,11 +1,9 @@
-const {uploadFile,downloadFile,deleteFile} = require("../services/s3")
+const {uploadFile,downloadFile,deleteFile,updatedFile} = require("../services/s3")
 const ErrorResponse = require("../utils/errorResponse")
 const httpCodes = require("../constants/httpCodes")
-const {getBuckets} = require("../services/s3")
+
 const uploadS3 = async (req,res,next)=>{
     const file = req.files.file
-    const buckets = await getBuckets()
-    console.log(buckets)
     uploadFile(file,(error,data)=>{
         if(error){
             return next(new ErrorResponse("Something went wrong.",httpCodes.BAD_REQUEST))
@@ -16,8 +14,9 @@ const uploadS3 = async (req,res,next)=>{
 
 const downloadS3 = async (req,res,next)=>{
     try {
-        const fileToSend = await downloadFile(req.body.filename)
-        fileToSend.pipe(res)
+        const fileToSend = await downloadFile(req.query.filename)
+        console.log(fileToSend)
+        fileToSend.pipe(res.download)
     } catch (error) {
         next(error)
     }
@@ -32,5 +31,21 @@ const deleteS3 = (req,res,next)=>{
     })
 }
 
+const updateS3 = (req,res,next)=>{
+    const {newKey,oldKey} = req.body
+    updatedFile(newKey,oldKey,(error,data)=>{
+        if(error){
+            return next(new ErrorResponse("Can not modify the file, Please try again later",httpCodes.BAD_REQUEST))
+        }
+        req.body.filename = oldKey
+        deleteS3(req.body.filename,(error,data)=>{
+            if(error){
+                return next(new ErrorResponse("Can not modify the file, Please try again later",httpCodes.BAD_REQUEST))
+            }
+            return res.status(httpCodes.OK).json({error:false,message:"File has been changed successfully"});
+        })
+    })
+}
 
-module.exports = {deleteS3,downloadS3,uploadS3}
+
+module.exports = {deleteS3,downloadS3,uploadS3,updateS3}
